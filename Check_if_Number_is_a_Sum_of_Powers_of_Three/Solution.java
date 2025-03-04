a677db07b0f7c746a417eec032bf575ab4930405
Tanmay Soni <tanmay.soni.09@gmail.com>
2025-03-04 11:39:08 -0500

    Daily Questions 4 March

---
+++ Check_if_Number_is_a_Sum_of_Powers_of_Three/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public static boolean checkPowersOfThree(int n) {
+        while (n > 0) {
+            // Check if this power should be used twice
+            if (n % 3 == 2) {
+                return false;
+            }
+            // Divide n by 3 to move to the next greater power
+            n /= 3;
+        }
+        // The ternary representation of n consists only of 0s and 1s
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPowersOfThree(12));
+    }
+}
